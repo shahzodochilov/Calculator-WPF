b8f4c211517973912b27aff756c3a12b464ecd0e
shahzodochilov <Shahzodxonochilov@gmail.com>
2022-11-01 22:55:21 +0500

    Create Project

---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool helper = false, helperRes = false, isoperator = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MinWidth = this.Width;
+            this.MinHeight = this.Height;
+        }
+
+        private void NumericButton_Click(object sender, RoutedEventArgs e)
+        {
+            isoperator = false;
+            Button button = (Button)sender;
+
+            if (ResultTbUI.Text == "0")
+                ResultTbUI.Text = "";
+
+            if (helper)
+            {
+                ResultTbUI.Text = "";
+                helper = false;
+            }
+
+            if (helperRes)
+            {
+                BeforeResultTbUI.Text = "";
+                helperRes = false;
+            }
+
+            if (button.Content.ToString() == ".")
+            {
+                if (ResultTbUI.Text.Contains('.'))
+                    return;
+                if (ResultTbUI.Text == "")
+                    ResultTbUI.Text = "0";
+            }
+            ResultTbUI.Text += button.Content;
+
+        }
+
+        public void Add()
+        {
+            string str = BeforeResultTbUI.Text + ResultTbUI.Text + " " + "=";
+            ResultTbUI.Text = (ExpressionEvaluator.ExpressionEvaluator.Evaluate(BeforeResultTbUI.Text + ResultTbUI.Text)).ToString();
+            BeforeResultTbUI.Text = str;
+            helper = true;
+            helperRes = true;
+            isoperator = false;
+        }
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            isoperator = false;
+            Button button = (Button)sender;
+
+            switch (button.Content.ToString())
+            {
+                case "C":
+                    BeforeResultTbUI.Text = "";
+                    ResultTbUI.Text = "0";
+                    break;
+
+                case "<-":
+                    if (ResultTbUI.Text.Length > 1)
+                        ResultTbUI.Text = ResultTbUI.Text[..^1];
+                    else if (ResultTbUI.Text.Length == 1) ResultTbUI.Text = "0";
+                    break;
+
+                case "+/-":
+                    if (ResultTbUI.Text != "0")
+                        ResultTbUI.Text = ResultTbUI.Text[0] == '-' ? ResultTbUI.Text[1..] : "-" + ResultTbUI.Text;
+                    break;
+
+                case "=":
+                    Add();                    
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MathematicalOperationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (isoperator)
+            {
+                BeforeResultTbUI.Text = BeforeResultTbUI.Text[..^1] + button.Content.ToString();
+            }
+
+            else
+            { 
+                 if(!helperRes)  Add();
+                BeforeResultTbUI.Text = ResultTbUI.Text + button.Content.ToString();
+                helper = true;
+                helperRes = false;
+                isoperator = true;      
+            }
+        }
+    }
+}
